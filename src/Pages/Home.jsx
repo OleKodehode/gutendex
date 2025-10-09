2b13a15ebbed18f6cc33521c411113c532c1e329
Ole Kodehode <ole.kodehode@gmail.com>
2025-10-09 14:50:15 +0200

    Changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import { getBooks, getSpecificBooks } from "../services/gutendex";
 import sample from "../../sample.json";
 
 export default function Home() {
-  const { results } = sample;
+  const { results, next, previous, count } = sample;
   const [books, setBooks] = useState(results);
 
   // useEffect(() => {
@@ -15,9 +15,16 @@ export default function Home() {
 
   return (
     <main>
-      {books.map((book) => {
-        return <Cards key={book.id} book={book} />;
-      })}
+      <section className="books-container">
+        {books.map((book) => {
+          return <Cards key={book.id} book={book} />;
+        })}
+      </section>
+      <section className="flex gap-5 items-center justify-center mt-5">
+        {!previous && <button className="nav-btn">Previous</button>}
+        {!next && <button className="nav-btn">Next</button>}
+      </section>
+      <p className="text-lg text-center mt-5">Fetched {count} books</p>
     </main>
   );
 }
