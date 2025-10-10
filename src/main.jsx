afcdb0d49fd304d20157b33569ede79e1cc69c27
Ole Kodehode <ole.Kodehode@gmail.com>
2025-10-10 20:15:36 +0200

    changes to hopefully fix deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import "./styles/index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./lib/routes.jsx";
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+  basename: process.env.NODE_ENV === "production" ? "/gutendex/" : "/",
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
